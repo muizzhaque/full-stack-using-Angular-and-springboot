417a6eaf5ba892d3d1c098aabc700a930b5557df
muizzhaque <113048786+muizzhaque@users.noreply.github.com>
2023-02-19 23:15:45 +0530

    Add files via upload

---
+++ backend/restful-web-services/restful-web-services/src/main/java/com/myproject/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -0,0 +1,48 @@
+package com.myproject.rest.webservices.restfulwebservices.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardcodedService {
+	private static List<Todo> todos = new ArrayList<>();
+	private static long idCounter=(long) 0;
+	static {
+		todos.add(new Todo(++idCounter,"muizz30520","Learn Cloud Computing",new Date(),false));
+		todos.add(new Todo(++idCounter,"muizz30520","Learn DevOps Now",new Date(),false));
+		todos.add(new Todo(++idCounter,"muizz30520","Learn AWS",new Date(),false));
+	}
+  public List<Todo> findAll(){
+	  return todos;
+  }
+  public Todo save(Todo todo) {
+	  if(todo.getId()==-1|| todo.getId()==0) {
+		  todo.setId(++idCounter);
+		  todos.add(todo);
+	  }else {
+		  deleteById(todo.getId());
+		  todos.add(todo);
+	  }
+	  return todo;
+  }
+  
+  public Todo deleteById(long id) {
+	  Todo todo =findById(id);
+	  if(todo==null) {
+		  return null;
+	  }
+	  todos.remove(todo);
+	  return todo;
+  }
+public Todo findById(long id) {
+	for(Todo todo:todos) {
+		if(todo.getId()==id) {
+			return todo;
+		}
+	}
+	return null;
+}
+}
